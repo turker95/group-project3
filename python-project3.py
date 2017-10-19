fb6b1262f888cb9288086ed2d03b61c3a4d048e4
turker95 <32176692+turker95@users.noreply.github.com>
2017-10-19 13:04:22 -0500

    Update python-project3.py

--- python-project3.py
+++ python-project3.py
@@ -1,3 +1,6 @@
 #import requests
 #import datetime 
-r = requests.get('https://s3.amazonaws.com/tcmg412-fall2016/http_access_log')
+LOG = requests.get('https://s3.amazonaws.com/tcmg412-fall2016/http_access_log')
+
+def main():
+  
