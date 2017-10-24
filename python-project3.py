08a07eeacf8e734219aca4f04e9155853a70e5ce
turker95 <32176692+turker95@users.noreply.github.com>
2017-10-24 13:21:10 -0500

    Update python-project3.py

--- python-project3.py
+++ python-project3.py
@@ -4,7 +4,22 @@ import re
 from datetime import datetime
 
 
-LOG = requests.get('https://s3.amazonaws.com/tcmg412-fall2016/http_access_log')
+i = 0
+by_month = {}
+
+print "Downloading log file ..."
+
+log_file = requsts.get('https://s3.amazonaws.com/tcmg412-fall2016/http_access_log')
+with open('http_log.txt', "w") as local:
+  local.write(response.read())
+
+print "Log file was saved ..."
+
+regex = re.compile(".*\[(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*")
+
+for line in open(http_log.txt
+
+
 
 def main():
   
